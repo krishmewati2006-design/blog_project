c8a082784406093b1b08b5f69b4cd9cb601f343d
krish_mewati <krishmewati2006@gmail.com>
2026-05-18 13:02:05 +0000

    Minor Change in entypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-gunicorn store.wsgi
+gunicorn blog_api.wsgi
